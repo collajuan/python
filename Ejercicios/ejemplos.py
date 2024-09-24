3b59788eae7a0004846ebc3a6fe8f6ebc79320b3
collajuan <collajuan@gmail.com>
2024-09-24 18:03:08 +0200

    validar_ip

--- Ejercicios/ejemplos.py
+++ Ejercicios/ejemplos.py
@@ -97,14 +97,30 @@ adn='ACGTTGGTAAgg'
 # adn_to_arn(adn)
 
 #TODO recibe un string y devuelve lista con el cuadrado de los numeros que encuentre
-def numeros_al_cuadrado(strin: str) -> list:
+def numeros_al_cuadrado(string: str) -> list:
     lista_cuadrados = []
-    lista_string = list(strin)
+    lista_string = list(string)
     print(lista_string)
     # print(type(lista_string))
     for n in lista_string:
          if n.isdigit():
-              lista_cuadrados.append(int(n)*int(n))
+              lista_cuadrados.append(int(n)**2)
     return lista_cuadrados        
 
-print(numeros_al_cuadrado('sdfsdf5fdg897'))
+# print(numeros_al_cuadrado('sdfsdf5fdg897'))
+
+#TODO validar IP
+from ipaddress import ip_address
+
+def validar_ip(ip: str) -> bool:
+    #! ip_address(input('indique una IP: ')))  aun no vimos try - except
+    numeros_ip=ip.split('.')
+    print(numeros_ip)
+    for numero in numeros_ip:
+        if (not 0<= int(numero) <= 255):
+            return 'IP no valida'
+    return 'IP valdia'
+
+print(validar_ip('192.1680.1.1'))
+
+#TODO 4 numeros narcisistas con 3 cifras 1**3 + 5**3 + 3**3 = 153
