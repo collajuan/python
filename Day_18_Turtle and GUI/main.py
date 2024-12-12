f230c8dbdf0b4d20a690427e7bbd2f00eeb3c9fd
collajuan <collajuan@gmail.com>
2024-12-12 12:14:45 -0300

    etch-a-sketch

---
+++ Day_18_Turtle and GUI/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+new_turtle = Turtle()
+new_turtle.shape('turtle')
+
+
+new_turtle.forward(100)
+new_turtle.right(90)
+new_turtle.forward(100)
+new_turtle.right(90)
+new_turtle.forward(100)
+new_turtle.right(90)
+new_turtle.forward(100)
+new_turtle.right(90)
+
+
+screen = Screen()
+screen.exitonclick()
+
