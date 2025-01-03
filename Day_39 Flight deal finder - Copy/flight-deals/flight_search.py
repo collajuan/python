47ee0ee026a6df84f1ce93fc43dbd5ff9d6967a8
collajuan <collajuan@gmail.com>
2025-01-03 20:52:21 -0300

    ignore

--- Day_39 Flight deal finder - Copy/flight-deals/flight_search.py
+++
@@ -1,7 +0,0 @@
-class FlightSearch:
-    # This class is responsible for talking to the Flight Search API.
-    def __init__(self):
-        pass
-
-    def get_iata_code(self, city):
-        return "TESTING"
