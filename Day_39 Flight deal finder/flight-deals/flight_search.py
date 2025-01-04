8f3912b132edec2fa19bd6c4ab28b32041140434
collajuan <collajuan@gmail.com>
2025-01-04 18:22:19 -0300

    cheapest_flight

--- Day_39 Flight deal finder/flight-deals/flight_search.py
+++ Day_39 Flight deal finder/flight-deals/flight_search.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+SEARCH_FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+SEARCH_IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
+
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
     def __init__(self):
@@ -12,11 +16,10 @@ class FlightSearch:
         self._token = self._get_new_token()
 
     def get_iata_code(self, city):
-        amadeus_endpoint = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
         params = {
             "keyword": city
         }
-        response = requests.get(url=amadeus_endpoint, headers={"Authorization": f"Bearer {self._token}"}, params=params)
+        response = requests.get(url=SEARCH_IATA_ENDPOINT, headers={"Authorization": f"Bearer {self._token}"}, params=params)
         response.raise_for_status()
         print(response.json()["data"][0]["iataCode"])
         return response.json()["data"][0]["iataCode"]
@@ -31,7 +34,36 @@ class FlightSearch:
             'client_id': self._api_key,
             'client_secret': self._api_secret
         }
-        response = requests.post(url="https://test.api.amadeus.com/v1/security/oauth2/token", headers=header, data=body)
+        response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
         print(response.json()["access_token"])
         print(response.json()["expires_in"])
         return response.json()["access_token"]
+    
+    def get_flight_data(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers={
+            "Authorization": f"Bearer {self._token}"
+        }
+        
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time,
+            "returnDate": to_time,
+            "nonStop": "true",
+            "currencyCode": "GBP",
+            "adults": 1,
+            "max": 10
+        }
+
+        response = requests.get(url=SEARCH_FLIGHT_ENDPOINT, headers=headers, params=query)
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            print("There was a problem with the flight search.\n"
+                  "For details on status codes, check the API documentation:\n"
+                  "https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api"
+                  "-reference")
+            print("Response body:", response.text)
+            return None
+
+        return response.json()
+        
--- Day_39 Flight deal finder/flight-deals/main.py
+++ Day_39 Flight deal finder/flight-deals/main.py
@@ -1,17 +1,49 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-
+import time
+from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
+
+ORIGIN_CITY_IATA = "LON"
 
 data_manager = DataManager()
-
 sheet_data = data_manager.get_data()
+flight_search = FlightSearch()
+flight_data = FlightData(
+    price="N/A",
+    origin_airport="N/A", 
+    destination_airport="N/A", 
+    out_date="N/A",
+    return_date="N/A"
+)
+
 print(sheet_data)
-if sheet_data[0]['iataCode'] == '':
-    flight_search = FlightSearch()    
-    for row in sheet_data:
+for row in sheet_data:
+    if row['iataCode'] == '':
         row['iataCode'] = flight_search.get_iata_code(row['city'])
-    data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
-    print(sheet_data)
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+        time.sleep(2)
+
+print(sheet_data)
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
+
+
+# **********************************
+
+tomorrow = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
+six_month_from_today = (datetime.now() + timedelta(days=(6 * 30))).strftime("%Y-%m-%d")
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination['city']}...")
+    flights = flight_search.get_flight_data(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    cheapest_flight = flight_data.find_cheapest_flight(flights)
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    # Slowing down requests to avoid rate limit
+    time.sleep(2)
+
