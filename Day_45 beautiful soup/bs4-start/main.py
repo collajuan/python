ebefa7f1ae986e13290867e103992fc38f0aaec3
collajuan <collajuan@gmail.com>
2025-01-07 10:58:24 -0300

    BeautifulSoup

---
+++ Day_45 beautiful soup/bs4-start/main.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+# *********** EJEMPLOS BeautifulSoup ***********************
+# with open("website.html") as file:
+#     contents = file.read()
+
+# soup = BeautifulSoup(contents, "html.parser")
+# print(soup.title.string)
+
+# all_anchor = soup.find_all(name="a")
+# print(all_anchor)
+
+# for tag in all_anchor:
+#     print(tag.getText())
+#     print(tag.get("href"))
+
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+
+# headings = soup.select(".heading")
+# print(headings)
+
+# **************** *Ejemplos sitios reales* ******************
